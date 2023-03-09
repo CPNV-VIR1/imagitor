f83c7ad218bcf2c1e81dafd7cab3f0d05b041914
Yannick Perret <contact@yannickperret.com>
2023-03-09 08:30:41 +0100

    added vitejs base configuration

--- public/public.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-    var menu = document.getElementById("language");
-    if (menu.style.display === "none") {
-        menu.style.display = "block";
-    } else {
-        menu.style.display = "none";
-    }
-}
