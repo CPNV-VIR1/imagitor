1fa5677f7d3ae6b835cf4bb2fc56e48e633299f7
Yannick Perret <contact@yannickperret.com>
2023-03-09 09:51:04 +0100

    somes basic design form

--- src/main.js
+++ src/main.js
@@ -1,17 +1,30 @@
 import './scss/main.scss';
 //import * from './helpers/function';
-import * as dotenv from 'dotenv'
-dotenv.config()
 
+const URL_API = import.meta.env.Vite_API_URL;
 
+console.log(URL_API)
 
 document.querySelector('#app').innerHTML = `
-  <main>
-    <h1>Hello Vite!</h1>
-    <div>
-      <form action="">
+  <header class="app__header">
+    <h1>Imagitor ©</h1>
+    <form>
+      <select name="langage" id="langage">
+        <option value="fr">Français</option>
+        <option value="en">Anglais</option>
+      </select>
+    </form>
+  </header>
+  <main class="app__contain">
+        <form action="" method="POST" class="app__contain__form">
+          <label for="textToImage">Texte à transformer en image</label>
+          <textarea name="textToImage" id="textToImage"></textarea>
+          <input type="submit" value="Transformer en image">
+        </form>
 
-      </form>
-    </div>
+
+        <div class="app__contain__image">
+          <img src="" alt="Image générée">
+        </div>
   </main>
 `
