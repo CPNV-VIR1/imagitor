552fab21898f85d0b79526ed316ff7bd65e4998c
Yannick Perret <contact@yannickperret.com>
2023-03-23 08:59:30 +0100

    change function for factoring and separate action

--- backend/controllers/imageController.js
+++ backend/controllers/imageController.js
@@ -1,8 +1,14 @@
 const fs = require('fs');
-const {loadImage, createCanvas} = require('canvas');
-const width = 1200
-const height = 650
+const { loadImage, createCanvas } = require('canvas');
+const imgWidth = 1200
+const imgHeight = 650
+const path = require('path');
 
+/**
+ * It returns a string of random characters of a specified length.
+ * @param length - The length of the string you want to generate.
+ * @returns A string of random characters.
+ */
 const randomCharacters = (length) => {
     let result = '';
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -13,50 +19,57 @@ const randomCharacters = (length) => {
     return result;
 }
 
-const loadImage = () => {
-    const imageName = randomCharacters(15);
-    loadImage(`./img/${imageName}`).then((data) => {
-        context.drawImage(data, 340, 515, 70, 70)
-        const imgBuffer = canvas.toBuffer('image/png')
-        fs.writeFileSync('./resources/images/drawnImage.png', imgBuffer)
-      })
+/* Saving the image to the images folder. */
+exports.saveImage = async (image, imageName) => {
+    return new Promise((resolve, reject) => {
+        const imagePath = path.join('backend', 'assets', 'images', imageName);
+        fs.writeFile(imagePath, image.toBuffer(), (err) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(imagePath);
+            }
+        });
+    });
 }
 
-exports.getImage = async (req, res) => {
-
-    try {
-        const image = await Image.findById(req.params.id);
-        if (!image) {
-            return res.status(404).json({ message: 'Image not found' });
+exports.getImageByName = async (imageName) => {
+    return new Promise((resolve, reject) => {
+      const imagePath = path.join('images', imageName);
+      fs.access(imagePath, fs.constants.F_OK, (err) => {
+        if (err) {
+          resolve(null);
+        } else {
+          resolve(imagePath);
         }
-        res.status(200).json(image);
-    } catch (error) {
-        res.status(500).json({ message: 'Server Error' });
-    }
+      });
+    });
+  }
+
+/* A function that is being exported to be used in another file. */
+exports.getAllImages = async () => {
+    return new Promise((resolve, reject) => {
+        fs.readdir('images', (err, files) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(files);
+            }
+        });
+    });
 }
 
-exports.createImage = async (req, res) => {
-    try {
-        const canvas = createCanvas(width, height)
-        const context = canvas.getContext('2d')
-        context.fillStyle = '#2b03a3'
-        context.fillRect(0, 0, width, height)
-        context.font = 'bold 72pt Menlo'
-        context.textBaseline = 'top'
-        context.textAlign = 'center'
-        context.fillStyle = '#f7ab07'
-        const imgText = res.body.image.textToImage
-        const textAlign = context.measureText(imgText).width
-        context.fillRect(590 - textAlign / 2 - 10, 170 - 5, textAlign + 20, 120)
-        context.fillStyle = '#ffffff'
-        context.fillText(imgText, 555, 120)
-        context.fillStyle = '#ffffff'
-        context.font = 'bold 32pt Menlo'
-        context.fillText('positronx.io', 755, 600)
+/* Creating a canvas with the text that is being passed in. */
+exports.createImage = async (text) => {
+    const canvas = createCanvas(imgWidth, imgHeight);
+    const ctx = canvas.getContext('2d');
 
-        const image = await Image.create(req.body);
-        res.status(201).json(image);
-    } catch (error) {
-        res.status(500).json({ message: 'Server Error' });
-    }
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    ctx.fillStyle = 'black';
+    ctx.font = '30px sans-serif';
+    ctx.fillText(text, 10, 100);
+
+    return canvas;
 }
