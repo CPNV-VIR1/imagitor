6cf60fe4b06362eec8e3ade8222d9ead47426b49
Yannick Perret <contact@yannickperret.com>
2023-03-23 08:57:33 +0100

    Change requêtes url and method. rework server for best practice

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,71 @@
-'use strict';
 const http = require('http');
-const { getImage, createImage } = require('./controllers/imageController');
+const querystring = require('querystring');
+const fs = require('fs');
+const { saveImage, getImageByName, getAllImages, createImage } = require('./controllers/imageController');
 
-const app = http.createServer((req, res) => {
-    res.writeHeader(200, 'Access-Control-Allow-Origin', 'http://127.0.0.1:5173');
-    res.writeHeader(200, 'Access-Control-Allow-Methods', 'OPTIONS', 'GET', 'POST');
-    res.writeHeader(200, 'Access-Control-Allow-Headers', 'Content-Type');
-    res.writeHeader(200, 'Access-Control-Max-Age', 2592000); // 30 days
+const port = 5000;
 
-    console.log()
+const server = http.createServer(async (req, res) => {
+    // CORS headers
+    res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5173');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-    if (req.url === '/image' && req.headers['access-control-request-method'] === 'GET') {
-        console.log('lflflfl');
+    // Preflight request. Reply successfully:
+    if (req.method === 'OPTIONS') {
+        res.writeHead(200);
+        res.end();
+        return;
+    }
 
-       /* if(req.body.image) {
-            console.log(req.params.id)
-        }*/
-        //getImage(req, res);
-    } else if (req.url === '/image' && req.headers['access-control-request-method'] === 'POST') {
-        console.log("flsmlsdmflk")
-        createImage(req, res);
+    if (req.url.startsWith('/image')) {
+
+        if (req.method === 'GET') {
+            if (req.url === '/image') {
+                const images = await getAllImages();
+                res.writeHead(200, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify(images));
+            } else {
+
+                const imageName = req.url.substring(7);
+                const imagePath = await getImageByName(imageName);
+
+                if (imagePath) {
+                    res.writeHead(200, { 'Content-Type': 'image/png' });
+                    res.end(fs.readFileSync(imagePath));
+                } else {
+                    res.writeHead(404, { 'Content-Type': 'text/plain' });
+                    res.end('Image introuvable');
+                }
+            }
+        } else if (req.method === 'POST') {
+            let body = '';
+            req.on('data', chunk => {
+                body += chunk.toString();
+            });
+
+            req.on('end', async () => {
+                const postData = querystring.parse(body);
+                const text = postData.text || 'Texte par défaut';
+
+                const image = await createImage(text);
+                const imageName = `${Date.now()}_${encodeURIComponent(text)}.png`;
+                const imagePath = await saveImage(image, imageName);
+
+                res.writeHead(200, { 'Content-Type': 'text/plain' });
+                res.end(imagePath);
+            });
+
+        } else {
+            res.writeHead(404, { 'Content-Type': 'text/plain' });
+            res.end('Seules les requêtes POST sont autorisées');
+        }
     } else {
-        res.writeHead(404, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify({ message: 'Route not found' }));
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Page introuvable');
     }
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(port, () => {
+    console.log(`Serveur démarré sur le port ${port}`);
+});
