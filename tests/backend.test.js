2ce2da841abd8520d07de0bbf2c3aba5039c7812
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2023-03-24 08:37:35 +0100

    Update backend.test.js

--- tests/backend.test.js
+++ tests/backend.test.js
@@ -1,30 +1,29 @@
-let ImageController = require('../backend/controllers/imageController.js');
+
 
 const { loadImage, createCanvas } = require('canvas');
-
-const imgWidth = 1200
-const imgHeight = 650
+const { saveImage, getImageByName, getAllImages, createImage } = require('../backend/controllers/imageController.js');
+const imgWidth = 1200;
+const imgHeight = 650;
 
 test('created image with inputs returns canvas', () => {
 
     //given
     const canvas = createCanvas(imgWidth, imgHeight);
-    var image = ImageController.createImage("test");
+    const image = createImage("test");
 
     //when
     //Assertion will trigger the events
-
-    expect(image).toEqual(canvas);
+    expect(image.imgWidth).toEqual(canvas.imgWidth);
     //then
 })
-test('created image without inputs returns canvas', () => {
+test('created image without inputs returns undefined', () => {
 
     //given
-    var image = ImageController.createImage("");
+    var image = createImage("");
 
     //when
     //Assertion will trigger the events
 
     //then
-    expect(image).toEqual(canvas);
+    expect(image.imgWidth).toEqual(undefined);
 })
