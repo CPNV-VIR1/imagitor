5343ff29aa9163316c91a3feb1e929967b4e1091
tristepin222 <48352287+JessyBorcard@users.noreply.github.com>
2023-03-29 11:11:29 +0200

    test: added tests

--- tests/backend.test.js
+++ tests/backend.test.js
@@ -1,55 +1,77 @@
-
-
 const { loadImage, createCanvas } = require('canvas');
-const { saveImage, getImageByName, getAllImages, createImage } = require('../backend/controllers/imageController.js');
+const { saveImage, getImageByName, getAllImages, createCanvasWithText } = require('../backend/controllers/imageController.js');
+const { Image } = require('../backend/models/image');
 const imgWidth = 1200;
 const imgHeight = 650;
 
-test('created image with inputs returns canvas', () => {
+test('created image with inputs returns canvas', async () => {
 
     //given
     const canvas = createCanvas(imgWidth, imgHeight);
-    const image = createImage("test");
+    const image = await createCanvasWithText("test");
 
     //when
     //Assertion will trigger the events
-    expect(image.imgWidth).toEqual(canvas.imgWidth);
+    expect(typeof image).toEqual(typeof canvas);
     //then
 })
-test('created image without inputs returns undefined', () => {
+test('created image without inputs returns empty canvas', () => {
 
     //given
-    var image = createImage("");
+    var image = createCanvasWithText("");
 
     //when
     //Assertion will trigger the events
 
     //then
-    expect(image.imgWidth).toEqual(undefined);
+    expect(typeof image).toEqual(typeof createCanvas());
 })
 
 test('Get Image returns Image', () => {
 
     
     //given
-    const image = createImage("test");
+    const image = createCanvasWithText("test");
 
     //when
     //Assertion will trigger the events
 
     //then
-    expect(image.imgWidth).toEqual(getImageByName("test").imgWidth);
+    expect(typeof image).toEqual(typeof getImageByName("test"));
 })
 
-test('Save Image returns Image name', () => {
+test('Save Image returns PathName', async() => {
 
 
     //given
-    const image = saveImage("test", "test");
+    const text = "test";
+    const canvas = await createCanvasWithText(text);
+    const pathName = await saveImage(canvas, "test");
+
+    const imageName = await Image.findOne({ where: { text } });
+
 
     //when
     //Assertion will trigger the events
 
     //then
-    expect(image).toEqual("test");
+    expect(pathName).toEqual(imageName.filename);
+})
+
+
+test('Post image via API with text', async () => {
+    
+
+
+})
+test('Post image via API without text', async () => {
+
+})
+
+
+test('Get Image via API with text', async () => {
+
+})
+test('Get Image via API without text', async () => {
+
 })
